30dd6bd0dfe53108425fc369bc19638cf06477ab
hanngorigo <hanngorigo@gmail.com>
2024-06-02 10:58:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_caching import Cache
+
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
-
-
-if __name__ == "__main__":
-    app.run()
+# tell Flask to use the above defined config
+app.config.from_mapping(config)
+cache = Cache(app)
